4c0edcbe52e64e6cff3fd1be548e392066d76bb1
Ikechukwu Nwachukwu <professionaltechman@gmail.com>
2024-04-21 18:26:28 +0100

    Filter out words that start with the letter 'A', regardless of case, from a list of strings

--- src/Filtering.elm
+++ src/Filtering.elm
@@ -32,3 +32,14 @@ filterShortWords words =
 filterHighScores : List Int -> List Int
 filterHighScores scores =
     List.filter (\score -> score > 75) scores
+
+
+
+--Exercise 4: Filter Out Words Starting with 'A'
+--Task: Create a function to filter out words that start with the letter 'A',
+--regardless of case, from a list of strings.
+
+
+startsWithA : List String -> List String
+startsWithA words =
+    List.filter (\word -> String.toLower (String.left 1 word) /= "a") words
