900c1981eabf2125ad221f92753dc740ec788626
Ikechukwu Nwachukwu <professionaltechman@gmail.com>
2024-04-21 17:31:05 +0100

    Filter words in a list such that the output excludes words shorter than 4 characters from a list of strings

--- src/Filtering.elm
+++ src/Filtering.elm
@@ -12,3 +12,13 @@ filterEvenIntegers integers =
 isOdd : Int -> Bool
 isOdd integer =
     modBy 2 integer /= 0
+
+
+
+--Exercise 2: Filter Short Words
+--Task: Create a function to exclude words shorter than 4 characters from a list of strings.
+
+
+filterShortWords : List String -> List String
+filterShortWords words =
+    List.filter (\word -> String.length word >= 4) words
